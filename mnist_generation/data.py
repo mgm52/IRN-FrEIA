cb077a8ae282a25329f773154886061e22abd8b7
Max McGuinness <mgm52@cam.ac.uk>
2021-11-20 01:09:16 +0000

    First FrEIA models

--- mnist_generation/data.py
+++ mnist_generation/data.py
@@ -1,1 +1,44 @@
 # Get dataset
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+from sklearn.datasets import make_moons
+from sklearn.datasets import load_digits
+
+def sample_mmoons():
+    data, labels = make_moons(n_samples=100, noise=0.05)
+    return data, labels
+
+def see_moons(data, labels=1):
+    plt.scatter(data[:, 0][labels==1], data[:, 1][labels==1], c="b")
+    plt.scatter(data[:, 0][labels==0], data[:, 1][labels==0], c="r")
+    plt.show()
+
+def see_example_moons():
+    data, labels = sample_mmoons()
+    print(f'Moon points: {data}')
+    print(f'Moon labels: {labels}')
+    see_moons(data, labels)
+
+
+mnist8_data, mnist8_labels = load_digits(return_X_y=True)
+def sample_mnist8():
+    i = random.randint(0, len(mnist8_data)-1)
+    data = np.array(mnist8_data[i])
+    label = mnist8_labels[i]
+
+    data.resize(8*8)
+    return data, label
+
+def see_mnist8(data, label=0):
+    print(f'Digit label: {label}')
+
+    im = np.array(data, dtype=np.uint8)
+    im.resize(8, 8)
+
+    plt.imshow(im)
+    plt.show()
+
+def see_example_mnist8():
+    data, label = sample_mnist8()
+    see_mnist8(data, label)
--- mnist_generation/main.py
+++ mnist_generation/main.py
@@ -1,1 +1,59 @@
 # Train a given model
+import data as data_functions
+import torch
+
+from networks.freia_nets import inn_fc
+from networks.freia_nets import inn_conv
+from networks.freia_nets import sample_inn_fc_mnist8
+from networks.freia_nets import sample_inn_conv_mnist8
+
+import numpy as np
+
+def train_inn_fc_mnist8(inn):
+    optimizer = torch.optim.Adam(inn.parameters(), lr=0.001)
+
+    for i in range(2000):
+        optimizer.zero_grad()
+        data, label = data_functions.sample_mnist8()
+
+        x = torch.Tensor(np.array([data]))
+        z, log_jac_det = inn(x)
+        loss = 0.5*torch.sum(z**2, 1) - log_jac_det
+        loss = loss.mean() / (8*8)
+
+        if i%100==99: print(f'Average loss across parameters: {loss}')
+        
+        loss.backward()
+        optimizer.step()
+
+# Conv version currently broken - need to sort out dims
+def train_inn_conv_mnist8(inn):
+    optimizer = torch.optim.Adam(inn.parameters(), lr=0.001)
+    #n_dim = (1, 8, 8)
+
+    for i in range(2000):
+        optimizer.zero_grad()
+        data, label = data_functions.sample_mnist8()
+
+        data.resize(1, 1, 8, 8)
+        x = torch.Tensor(data)
+        z, log_jac_det = inn(x)
+        loss = 0.5*torch.sum(z**2, 1) - log_jac_det
+        loss = loss.mean() / (8*8)
+
+        if i%100==99: print(f'Average loss across parameters: {loss}')
+        
+        loss.backward()
+        optimizer.step()
+
+def full_inn_fc_mnist8_example():
+    inn = inn_fc()
+    train_inn_fc_mnist8(inn)
+    sample_inn_fc_mnist8(inn, 5)
+
+def full_inn_conv_mnist8_example():
+    inn = inn_conv()
+    train_inn_conv_mnist8(inn)
+    sample_inn_conv_mnist8(inn, 5)
+
+full_inn_conv_mnist8_example()
