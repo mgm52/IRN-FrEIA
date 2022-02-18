f86484913adc19dc9de5645d0db7986e35fbbe6f
M.G. McGuinness <mgm52@login-e-2.data.cluster>
2022-02-18 02:04:36 +0000

    Adjustments

--- image_rescaling/data2.py
+++ image_rescaling/data2.py
@@ -9,6 +9,9 @@ import matplotlib.pyplot as plt
 from bicubic_pytorch.core import imresize
 import numpy as np
 
+#training_data = Div2K(root="./data", scale=4, split="train", track="bicubic", transform=ToTensor(), download=False)
+#testing_data = Div2K(root="./data", scale=2, split="test", track="bicubic", transform=ToTensor(), download=True)
+
 class DataLoaders:
     def __init__(self, train_dataloader, test_dataloader, sample_shape):
         self.train_dataloader: DataLoader = train_dataloader
@@ -18,16 +21,13 @@ class DataLoaders:
         self.sample_shape = sample_shape
 
 def Div2KDataLoaders(batch_size, img_size=64, shuffle_training_data=True):
-    #training_data = Div2K(root="./data", scale=4, split="train", track="bicubic", transform=ToTensor(), download=False)
-    #testing_data = Div2K(root="./data", scale=8, split="test", track="bicubic", transform=ToTensor(), download=False)
-
     transform = transforms.Compose([transforms.CenterCrop(img_size), transforms.ToTensor()])
-    dataset = datasets.ImageFolder("./data/DIV2K/DIV2K_train_LR_x2", transform=transform)
+    dataset = datasets.ImageFolder("/rds/user/mgm52/hpc-work/invertible-image-rescaling/data/DIV2K/DIV2K_train_LR_x2", transform=transform)
     train_dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle_training_data)
     print(f"Loaded {len(dataset)} training images")
 
     transform = transforms.Compose([transforms.CenterCrop(img_size), transforms.ToTensor()])
-    dataset = datasets.ImageFolder("./data/DIV2K/DIV2K_valid_LR_x2", transform=transform)
+    dataset = datasets.ImageFolder("/rds/user/mgm52/hpc-work/invertible-image-rescaling/data/DIV2K/DIV2K_test_LR_x2", transform=transform)
     test_dataloader = DataLoader(dataset, batch_size=len(dataset), shuffle=False)
     print(f"Loaded {len(dataset)} test images")
 
